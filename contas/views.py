b1fbd5e3e52df8efe17f677123ae655dbf3aad4b
Karin <karindeoliveira@ibm.com>
2022-01-08 19:28:39 -0300

    Criando uma listagem de dados

--- contas/views.py
+++ contas/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from contas.models import Transacao
+
 
 from django.http import HttpResponse
 import datetime
@@ -15,3 +17,9 @@ def home(request):
 
 #data é um dicionario que ira repassar as infos para o html
 #django jinja que faz a implementação no html o python
+
+def listagem(request):
+    data = {}
+    data ['transacoes'] = Transacao.objects.all()
+    #Objects é um manager é uma classe de model e traz operações de banco de dados como o all para buscar tudo no BD, FILTER PARA FILTRAR ALGO, last para pegar a ultima transacao do bd, first para pegar a primeira transacao do bd,
+    return render(request, 'contas/listagem.html', data)
