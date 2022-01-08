616d5b23bb5aa2731f85922a06ca280ebc2bbd59
Karin <karindeoliveira@ibm.com>
2022-01-08 19:06:48 -0300

    Templates do django

--- contas/views.py
+++ contas/views.py
@@ -4,6 +4,14 @@ from django.http import HttpResponse
 import datetime
 
 def home(request):
-    now = datetime.datetime.now()
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+
+    data ['now'] = datetime.datetime.now()
+    #variavel python dentro do template html
+
    # html = "<html><body>It is now %s.</body></html>" % now
-    return render(request,'contas/home.html')
+    return render(request,'contas/home.html', data)
+
+#data é um dicionario que ira repassar as infos para o html
+#django jinja que faz a implementação no html o python
