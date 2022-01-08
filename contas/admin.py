1bb8886a0b416a4d85e0cb09b3eb5ce28e9cb4ca
Karin <karindeoliveira@ibm.com>
2022-01-08 18:15:39 -0300

    Persistencia no BD

--- contas/admin.py
+++ contas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from contas.models import Categoria
+
+admin.site.register(Categoria)
+#vai fazer a tabela no site e vai persistir os dados no bd. Estilo JPA
--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.https://docs.djangoproject.com/en/4.0/ref/models/fields/
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    dt_criacao = models.DateTimeField(auto_now_add=True)
+
